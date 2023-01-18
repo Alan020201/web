bf932ca26e59b3830d36313371d0446f824531fd
Alan Shah <np03cs4aheraldcollege.edu.np.com>
2023-01-18 11:27:18 +0545

    layout assignment

---
+++ week5/week5assingment/first.py
@@ -0,0 +1,11 @@
+fname = input("input first name : ")
+lname = input("input last name : ")
+print("hi " + lname + " " + fname)
+num1 = 45.45
+num2 = 69.68
+sum = num1+num2
+print('sum of {0} and {1} is {2}'.format(num1, num2, sum))
+num1 = input('first num:')
+num2 = input('sec num:')
+sum = float(num1) + float(num2)
+print('sum of {} and {} is {}'.format(num1, num2, sum))
